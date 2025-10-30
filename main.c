19f82fc8e6a009f031087854c4ae8f7fd05fa070
DylanO9 <dylan.olivares11@gmail.com>
2025-10-30 10:20:14 +0000

    feat: add a simple set up for functions used in the snake game

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct Snake {
+    int size;
+    int apples_eaten;
+};
+
+int setup_board();
+int process_input();
+int move_snake();
+int eat_apple();
+
+int main() {
+
+
+}
+
+int setup_board() {
+    
+}
+
+int process_input() {
+    
+}
+
+int move_snake() {
+
+}
+
+int eat_apple() {
+
+}
